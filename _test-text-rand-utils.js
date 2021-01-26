7fe4e7668b0ee8ec47cdb17d6d37f4e4e66993b0
fsd66 <fsd66@rocketmail.com>
2021-01-26 05:06:10 -0500

    add aestheticizer

--- _test-text-rand-utils.js
+++ _test-text-rand-utils.js
@@ -1,7 +1,9 @@
+const aestheticizer = require("./lib/aestheticizer/_test");
 const capitalizer = require("./lib/capitalizer/_test");
 const blockTextifyer = require("./lib/discord/_test");
 
 function runAllTests() {
+    aestheticizer.runTests();
     capitalizer.runTests();
     blockTextifyer.runTests();
 };
--- text-rand-utils.js
+++ text-rand-utils.js
@@ -1,5 +1,6 @@
 "use strict";
 
+const Aestheticizer = require("./lib/aestheticizer/aestheticizer");
 const Capitalizer = require("./lib/capitalizer/capitalizer");
 const BlockTextifyer = require("./lib/discord/block-textifyer");
 const Discord = { BlockTextifyer };
