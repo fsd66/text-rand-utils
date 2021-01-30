710a9e55d04ac1f513e3a565e549ac07ced5c3e9
fsd66 <fsd66@rocketmail.com>
2021-01-30 15:59:50 -0500

    pekopekopekopekopekopeko

--- _test-text-rand-utils.js
+++ _test-text-rand-utils.js
@@ -1,11 +1,13 @@
 const aestheticizer = require("./lib/aestheticizer/_test");
 const capitalizer = require("./lib/capitalizer/_test");
 const blockTextifyer = require("./lib/discord/_test");
+const pekofyer = require("./lib/pekofyer/_test");
 
 function runAllTests() {
     aestheticizer.runTests();
     capitalizer.runTests();
     blockTextifyer.runTests();
+    pekofyer.runTests();
 };
 
 runAllTests();
