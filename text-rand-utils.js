d0f5b71d627faaa44dd2586b1f76582a70cdc57e
arctan <arctanch@gmail.com>
2022-05-23 03:33:45 -0400

    sus forgotten change

--- text-rand-utils.js
+++ text-rand-utils.js
@@ -5,11 +5,13 @@ const Capitalizer = require("./lib/capitalizer/capitalizer");
 const BlockTextifyer = require("./lib/discord/block-textifyer");
 const Pekofyer = require("./lib/pekofyer/pekofyer");
 const Reversifyer = require("./lib/reversifyer/reversifyer");
+const Amogusizer = require("./lib/amogusizer/amogusizer");
 
 const Discord = { BlockTextifyer };
 
 module.exports = {
     Aestheticizer,
+    Amogusizer,
     Capitalizer,
     Discord,
     Pekofyer,
